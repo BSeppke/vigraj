83f39047b25a40f1b067053fb8433a2af9aafe89
bseppke <benjamin.seppke@gmail.com>
2017-09-04 14:30:08 +0200

    added clipping support for exporting images

--- Examples.java
+++ Examples.java
@@ -31,19 +31,20 @@ public class Examples
 					System.out.println("");
 				}
 				
-				float[] red = {255, 0, 0};
+				float[] red = {300, 0, 0};
 				
-				Impex.exportImage(
-					ImgProc.paddImage(
+				Image img2 = ImgProc.paddImage(
 						Filters.gaussianSmoothing(
-							ImgProc.resizeImage(
-								ImgProc.rotateImage(
-									ImgProc.reflectImage(img, 1),
-									30, 2),
-								(int)(img.getWidth()*1.1), (int)(img.getHeight()*1.1), 2),
-							2.0f),
-						10, 20, 30, 40, red),
-					Config.libDir() +"/images/bla.png");
+								ImgProc.resizeImage(
+									ImgProc.rotateImage(
+										ImgProc.reflectImage(img, 1),
+										30, 2),
+									(int)(img.getWidth()*1.1), (int)(img.getHeight()*1.1), 2),
+								2.0f),
+							10, 20, 30, 40, red);
+
+				Impex.exportImage(img2, Config.libDir() +"/images/bla-rescaled.png");
+				Impex.exportImage(img2, Config.libDir() +"/images/bla-clipped.png", false); //Use clipping 0..255 instead of rescaling
 			}
 			catch(Exception ex)
 			{
--- Impex.java
+++ Impex.java
@@ -15,9 +15,9 @@ public class Impex
         int vigra_importrgbimage_c(Pointer p_r, Pointer p_g, Pointer p_b, int width, int height, String filename);
         int vigra_importrgbaimage_c(Pointer p_r, Pointer p_g, Pointer p_b, Pointer p_a, int width, int height, String filename);
 
-        int vigra_exportgrayimage_c(Pointer p, int width, int height, String filename);
-        int vigra_exportrgbimage_c(Pointer p_r, Pointer p_g, Pointer p_b, int width, int height, String filename);
-        int vigra_exportrgbaimage_c(Pointer p_r, Pointer p_g, Pointer p_b, Pointer p_a, int width, int height, String filename);
+        int vigra_exportgrayimage_c(Pointer p, int width, int height, String filename, boolean rescale_range);
+        int vigra_exportrgbimage_c(Pointer p_r, Pointer p_g, Pointer p_b, int width, int height, String filename, boolean rescale_range);
+        int vigra_exportrgbaimage_c(Pointer p_r, Pointer p_g, Pointer p_b, Pointer p_a, int width, int height, String filename, boolean rescale_range);
     }
     
     public static Image importImage(String filename) throws IOException
@@ -79,7 +79,12 @@ public class Impex
         }
     }   
 	
-	public static void exportImage(Image img, String filename) throws IOException
+    public static void exportImage(Image img, String filename) throws IOException
+    {
+    	exportImage(img, filename, true); //Call with rescaling from min...max -> 0...255
+    }
+    
+    public static void exportImage(Image img, String filename, boolean rescale_range) throws IOException
     {
         int width    = img.getWidth();
         int height   = img.getHeight();
@@ -89,21 +94,21 @@ public class Impex
         {
         	if(numbands == 1)
         	{
-                if(CLibrary.INSTANCE.vigra_exportgrayimage_c(img.getBand(0), width, height, filename) != 0)
+                if(CLibrary.INSTANCE.vigra_exportgrayimage_c(img.getBand(0), width, height, filename, rescale_range) != 0)
 				{
 					throw new IOException("Gray image export failed!");
     	    	}
         	}
         	else if(numbands == 3)
         	{
-                if(CLibrary.INSTANCE.vigra_exportrgbimage_c(img.getBand(0), img.getBand(1), img.getBand(2), width, height, filename) != 0)
+                if(CLibrary.INSTANCE.vigra_exportrgbimage_c(img.getBand(0), img.getBand(1), img.getBand(2), width, height, filename, rescale_range) != 0)
 				{
 				   throw new IOException("RGB image export failed!");
     	    	}
         	}
         	else if(numbands == 3)
         	{
-                if(CLibrary.INSTANCE.vigra_exportrgbaimage_c(img.getBand(0), img.getBand(1), img.getBand(2), img.getBand(3), width, height, filename) != 0)
+                if(CLibrary.INSTANCE.vigra_exportrgbaimage_c(img.getBand(0), img.getBand(1), img.getBand(2), img.getBand(3), width, height, filename, rescale_range) != 0)
 				{
 				   throw new IOException("RGBA image export failed!");
     	    	}
